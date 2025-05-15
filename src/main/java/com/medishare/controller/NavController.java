07e7e3e6917f239b6044bd61de9c56f987d41a1d
Inari Goro <qqnq7um9k@gmail.com>
2025-05-15 16:20:29 +0900

    renamed LoginController.java to NavController.java and updated it for preparation of register_account.html

--- src/main/java/com/medishare/controller/NavController.java
+++ src/main/java/com/medishare/controller/NavController.java
@@ -4,9 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class NavController {
     @GetMapping("/login")
     public String login(){
         return "login";
     }
+
+    @GetMapping("/register_account")
+    public String register_account(){ return "register_account"; }
 }

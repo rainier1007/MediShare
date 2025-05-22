15835597f4caf87691c04c4c3c60fecc9ba37f69
Inari Goro <qqnq7um9k@gmail.com>
2025-05-22 16:35:22 +0900

    implemented the password remember and the password-open-hide feature in the login screen

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -3,6 +3,14 @@ const dialog = document.getElementById("loading-dialog");
 const dialogLoader = document.getElementById("loading-cat");
 const loadingMessage = document.getElementById("loading-message");
 
+const userInput = document.getElementById("username");
+const rememberUsername = document.getElementById("remember-username");
+
+const passwordInput = document.getElementById("password");
+const showPasswordButton = document.getElementById("password-eye");
+const eyeOpenPath = showPasswordButton.dataset.eyeOpenPath;
+const eyeClosePath = showPasswordButton.dataset.eyeClosePath;
+
 function showDialog(event){
     showLoadingDialog();
 }
@@ -14,4 +22,42 @@ function showLoadingDialog() {
     setTimeout(() => {
         loadingMessage.textContent = "サーバーに接続中...";
     }, 3000);
+}
+
+window.addEventListener("load", function() {
+    const savedUsername = localStorage.getItem("username");
+
+    if (savedUsername) {
+        userInput.value = savedUsername;
+        rememberUsername.checked = true;
+    }
+
+    rememberUsername.addEventListener("change", function() {
+        if (rememberUsername.checked) {
+            localStorage.setItem("username", userInput.value);
+        } else {
+            localStorage.removeItem("username");
+        }
+    });
+
+    userInput.addEventListener("input", function() {
+        localStorage.setItem("username", userInput.value);
+    });
+});
+
+showPasswordButton.addEventListener("click", showPassword);
+
+function showPassword(){
+    if(passwordInput.type === "password"){
+        passwordInput.type = "text";
+        passwordInput.style.fontSize = "20px"
+        showPasswordButton.src = eyeOpenPath;
+    }else{
+        passwordInput.type = "password";
+        showPasswordButton.src = eyeClosePath;
+    }
+
+    if(passwordInput.value.length > 0){
+        passwordInput.setSelectionRange(passwordInput.value.length, passwordInput.value.length);
+    }
 }

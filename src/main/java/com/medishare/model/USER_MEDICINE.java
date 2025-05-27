fe10e8fb80c494f956b101d265bf74e5acd4de27
rainier1007 <gawrguraomo@gmail.com>
2025-05-27 10:21:04 +0900

    USER_MEDICINEモデルの追加とUSER_DATABASEモデルの修正

---
+++ src/main/java/com/medishare/model/USER_MEDICINE.java
@@ -0,0 +1,53 @@
+package com.medishare.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name = "user_medicine")
+@Entity
+@Getter
+@Setter
+public class USER_MEDICINE {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userMedicineId;
+
+    // 🔗 外部キー（USER_DATABASEのuserIdと紐づけ）
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private USER_DATABASE user;
+
+    @Column(nullable = false)
+    private String medicineOfficialName;
+
+    @Column
+    private String medicineUserInput;
+
+    @Column
+    private String prescriptionDays;
+
+    @Column
+    private String prescriptionPerDay;
+
+    @Column
+    private int medicationMethod;
+
+    @Column
+    private String userComment;
+
+    public USER_MEDICINE() {
+    }
+
+    public USER_MEDICINE(USER_DATABASE user, String medicineUserInput, String medicineOfficialName, String prescriptionDays,
+                         String prescriptionPerDay, int medicationMethod, String userComment) {
+        this.user = user;
+        this.medicineUserInput = medicineUserInput;
+        this.medicineOfficialName = medicineOfficialName;
+        this.prescriptionDays = prescriptionDays;
+        this.prescriptionPerDay = prescriptionPerDay;
+        this.medicationMethod = medicationMethod;
+        this.userComment = userComment;
+    }
+}

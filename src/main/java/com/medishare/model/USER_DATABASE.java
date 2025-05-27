aa94cb7552e3130dfb18534b3d16cb94971944fd
rainier1007 <gawrguraomo@gmail.com>
2025-05-27 10:21:51 +0900

    USER_MEDICINEモデルの追加とUSER_DATABASEモデルの修正

---
+++ src/main/java/com/medishare/model/USER_DATABASE.java
@@ -0,0 +1,40 @@
+package com.medishare.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Table(name = "user_database")
+@Entity
+@Getter
+@Setter
+public class USER_DATABASE {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(nullable = false, unique = true)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = true)
+    private String familyEmail;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<USER_MEDICINE> medicines;
+
+    public USER_DATABASE(){
+
+    }
+    public USER_DATABASE(String userEmail, String password, String familyEmail){
+        this.userEmail = userEmail;
+        this.password = password;
+        this.familyEmail = familyEmail;
+    }
+
+}
